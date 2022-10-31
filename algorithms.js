364cd0373b7b780b83af0b7833e240da27087f00
Shakhbozbek Usmonov <83509440+shakhbozbekusmonov@users.noreply.github.com>
2022-10-31 17:09:28 +0500

    missingChar solved

--- algorithms.js
+++ algorithms.js
@@ -174,3 +174,12 @@ function notString(str){
 }
 
 console.log(notString("not x"));
+
+// Warmup 1.10
+// function missingChar(str, n){
+//     let str1 = str.slice(0, n);
+//     let str2 = str.slice(n + 1,);
+//     return str1 + str2;
+// }
+
+console.log(missingChar("kitten", 3));
