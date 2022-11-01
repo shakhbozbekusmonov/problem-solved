de4d27cb2bb9d509456a976dd1188f899058cb09
Shakhbozbek Usmonov <83509440+shakhbozbekusmonov@users.noreply.github.com>
2022-11-01 17:27:42 +0500

    monkey_trouble solved py

--- algorithms.py
+++ algorithms.py
@@ -15,4 +15,16 @@ def sleep_in(week_day, vacation):
     return (not week_day and not vacation) or (not week_day and vacation)
 
 print(sleep_in(False, False))
-    
+
+
+# warmupt 1.2
+# def monkey_trouble(aSmile, bSmile):
+#     if (aSmile and bSmile) or (not aSmile and not bSmile):
+#         return True;
+#     else:
+#         return False;
+
+def monkey_trouble(aSmile, bSmile):
+    return (aSmile and bSmile) or (not aSmile and not bSmile)
+
+print(monkey_trouble(True, True));
